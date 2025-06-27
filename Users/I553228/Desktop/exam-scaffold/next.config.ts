5c60a31137e23a7988d459f192b1255c6dfac9f0
GOSPEL ONONWI <Spellz49@gmail.com>
2025-06-27 23:48:45 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ Users/I553228/Desktop/exam-scaffold/next.config.ts
@@ -0,0 +1,29 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      '6000-firebase-studio-1748565680021.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev',
+    ],
+  },
+};
+
+export default nextConfig;
